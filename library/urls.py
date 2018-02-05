fdf631ddd1ab3a261ca5fefa1299cc14a47143bb
Moahmed Omran <mohamedgomran@gmail.com>
2018-02-05 10:38:53 +0200

    static files

---
+++ library/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, re_path
+from . import views
+
+app_name = 'library'
+
+urlpatterns = [
+    re_path(r'^book/([0-9]+)/$', views.book),
+    # re_path(r'^author/$', views.all_writer_view),
+]
--- library/views.py
+++ library/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from library.models import Book
+
 
 # Create your views here.
+def book(request, Id):
+	# book = Book.objects.get(id = Id)
+	book = ""
+	return render(request, 'library/Book.html', {"book":book})
+	
