fc107d9f79a49f60f468ac8741b36af89a6de7ae
Moahmed Omran <mohamedgomran@gmail.com>
2018-02-07 23:23:07 +0200

    favorite cat

--- library/models.py
+++ library/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Category(models.Model):
 	"""docstring for Category"""
 	name = models.CharField(max_length=(30))
+	user = models.ManyToManyField(User)
 
 
 class Book(models.Model):
